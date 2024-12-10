54ad479e067bb0f35633d652a4d40212864d3f52
Joel Campos <joelcamposoliva9000@gmail.com>
2024-12-10 15:39:50 +0100

    feat: :wrench: enable import aliases on client

--- apps/client/src/app.tsx
+++ apps/client/src/app.tsx
@@ -1,10 +1,8 @@
 import "./app.pcss";
-import * as pk from "../package.json";
 
 export default function App() {
   return (
     <div className="w-screen h-screen justify-center items-center flex flex-col gap-2">
-      <p>{pk.name}</p>
       <h1 className="text-6xl font-bold">NestFlux</h1>
       <p>Client scaffold</p>
     </div>
--- apps/client/vite.config.ts
+++ apps/client/vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      "@pkg": path.resolve(__dirname, "package.json"),
+      "@components": path.resolve(__dirname, "src/components"),
+      "@utils": path.resolve(__dirname, "src/utils"),
+      "@assets": path.resolve(__dirname, "src/assets"),
+    },
+  },
+});
