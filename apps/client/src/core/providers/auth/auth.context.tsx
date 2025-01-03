cd0a198e59080005e76c82761c265bca80237f1c
Joel Campos <joelcamposoliva9000@gmail.com>
2025-01-03 17:38:11 +0100

    feat: implement zod schema for auth context info and enhance session storage handling

--- apps/client/src/core/providers/auth/auth.context.tsx
+++ apps/client/src/core/providers/auth/auth.context.tsx
@@ -1,12 +1,15 @@
 import { ProviderSetter } from "@core/providers/provider-setter.type";
-import { UserModel } from "@shared/models";
+import { USER_SCHEMA } from "@shared/models";
 import { createContext } from "react";
+import { object, string, z } from "zod";
 
 export const AUTH_CONTEXT = createContext<ProviderSetter<AuthContextInfo>>(
   null!
 );
 
-export interface AuthContextInfo {
-  token: string;
-  user: UserModel;
-}
+export const AUTH_CONTEXT_INFO_SCHEMA = object({
+  token: string(),
+  user: USER_SCHEMA,
+});
+
+export type AuthContextInfo = z.infer<typeof AUTH_CONTEXT_INFO_SCHEMA>;
--- apps/client/src/core/providers/auth/auth.provider.tsx
+++ apps/client/src/core/providers/auth/auth.provider.tsx
@@ -1,17 +1,23 @@
 import Auth from "@core/components/auth/auth";
 import {
   AUTH_CONTEXT,
+  AUTH_CONTEXT_INFO_SCHEMA,
   AuthContextInfo,
 } from "@core/providers/auth/auth.context";
 import { useStoredAccounts } from "@core/providers/auth/stored-account.context";
 import { WithChildren } from "@core/types/common/component.types";
 import { useState } from "react";
 
+const SESSION_STORAGE_KEY = "active-auth-info";
+
 export default function AuthProvider({ children }: Readonly<WithChildren>) {
-  const [context, setContextState] = useState<AuthContextInfo>();
+  const [context, setContextState] = useState<AuthContextInfo | null>(
+    readCurrentAuthData()
+  );
   const { addAccount } = useStoredAccounts();
 
   const setContext = (info: AuthContextInfo) => {
+    setCurrentAuthData(info);
     addAccount(info);
     setContextState(info);
   };
@@ -24,3 +30,20 @@ export default function AuthProvider({ children }: Readonly<WithChildren>) {
     </AUTH_CONTEXT.Provider>
   );
 }
+
+const readCurrentAuthData = (): AuthContextInfo | null => {
+  try {
+    const rawData = sessionStorage.getItem(SESSION_STORAGE_KEY);
+    if (!rawData) return null;
+
+    const parsed = JSON.parse(rawData);
+
+    return AUTH_CONTEXT_INFO_SCHEMA.parse(parsed);
+  } catch {
+    return null;
+  }
+};
+
+const setCurrentAuthData = (data: AuthContextInfo) => {
+  sessionStorage.setItem(SESSION_STORAGE_KEY, JSON.stringify(data));
+};
