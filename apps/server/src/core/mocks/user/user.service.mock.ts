01e362d7bdf297b7d0fc3947cb194b192ee26234
Joel Campos <joelcamposoliva9000@gmail.com>
2025-01-05 10:59:10 +0100

    feat: update shared mocks package to improve API definition and restructure dependencies

--- apps/server/src/core/mocks/user/user.service.mock.ts
+++ apps/server/src/core/mocks/user/user.service.mock.ts
@@ -1,5 +1,5 @@
 import { UserService } from "@core/api/user/user.service";
-import { DB_USERS_MOCK } from "@shared/mocks/dist";
+import { DB_USERS_MOCK } from "@shared/mocks";
 
 export const USER_SERVICE_MOCK: UserService = Object.assign({
   findByUsername: async (username: string) => {
