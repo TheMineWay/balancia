817c8ab2c24fa48d02056999e6420e504834c101
Joel Campos Oliva <joelcamposoliva9000@gmail.com>
2025-02-23 09:31:28 +0100

    fix: :bug: use more complex hashing algorithm

--- apps/server/src/core/utils/cryptography/password-hashing.util.ts
+++ apps/server/src/core/utils/cryptography/password-hashing.util.ts
@@ -1,13 +1,13 @@
 import { createHash, randomBytes, scryptSync, timingSafeEqual } from "crypto";
 
 export function hash(text: string): string {
-  return createHash("sha256").update(text).digest("base64");
+  return createHash("sha3-512").update(text).digest("base64");
 }
 
 export function hashWithSalt(password: string): string {
   const salt: string = randomBytes(16).toString("base64");
   const hashedPassword: string = scryptSync(password, salt, 64).toString(
-    "base64",
+    "base64"
   );
 
   return `${salt}:${hashedPassword}`;
@@ -15,14 +15,14 @@ export function hashWithSalt(password: string): string {
 
 export function compareHash(
   originalPassword: string,
-  strangePassword: string,
+  strangePassword: string
 ): boolean {
   return hash(originalPassword) === hash(strangePassword);
 }
 
 export function compareHashWithSalt(
   originalPassword: string,
-  strangePassword: string,
+  strangePassword: string
 ): boolean {
   const [salt, key]: string[] = originalPassword.split(":");
 
