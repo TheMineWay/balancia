d0dac6b10901facbd1a411d287a355d2b923111a
Joel Campos Oliva <joelcamposoliva9000@gmail.com>
2025-06-29 09:36:35 +0200

    fix: styles & remove user profile

--- apps/client/src/features/core/auth/token/components/token-expiracy-modal.tsx
+++ apps/client/src/features/core/auth/token/components/token-expiracy-modal.tsx
@@ -1,13 +1,17 @@
 import { SignInButton } from "@common/core/auth/components/actions/sign-in-button";
+import { useLogout } from "@common/core/auth/hooks/use-logout";
 import { useOidcState } from "@common/core/auth/hooks/use-oidc-state";
 import { Modal } from "@mantine/core";
 
 export const TokenExpiracyModal: FC = () => {
   const { isTokenExpired } = useOidcState();
+  const { logout } = useLogout();
 
   return (
-    <Modal opened={isTokenExpired} onClose={() => {}}>
-      <SignInButton />
+    <Modal opened={isTokenExpired} onClose={logout}>
+      <div className="flex flex-col items-center justify-center">
+        <SignInButton />
+      </div>
     </Modal>
   );
 };
--- apps/client/src/i18n/locales/en-us/locale.ts
+++ apps/client/src/i18n/locales/en-us/locale.ts
@@ -1,14 +1,12 @@
 import auth from "./auth.json";
 import common from "./common.json";
 import errors from "./errors.json";
-import userProfile from "./user/user-profile.json";
 // End core
 
 const EN_US = {
   common,
   auth,
   errors,
-  userProfile,
   // End core
 };
 
