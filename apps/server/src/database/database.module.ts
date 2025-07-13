6769977a37992d80b23868d61104afc90231222a
Joel Campos Oliva <joelcamposoliva9000@gmail.com>
2025-07-13 09:58:12 +0200

    feat: add query logger

--- apps/server/src/database/database.module.ts
+++ apps/server/src/database/database.module.ts
@@ -2,7 +2,8 @@ import { ENV } from "@constants/conf/env.constant";
 import { DATABASE_PROVIDERS } from "@database/database.provider";
 import { DatabaseService } from "@database/services/database.service";
 import { DatabaseSeederService } from "@database/services/seeders/database-seeder.service";
-import { Global, Module } from "@nestjs/common";
+import { Global, Logger, Module } from "@nestjs/common";
+import { Logger as DbLogger } from "drizzle-orm";
 import { drizzle } from "drizzle-orm/mysql2";
 
 @Global()
@@ -11,7 +12,9 @@ import { drizzle } from "drizzle-orm/mysql2";
     {
       provide: DATABASE_PROVIDERS.main,
       useFactory: () => {
-        const db = drizzle(ENV.database.url);
+        const db = drizzle(ENV.database.url, {
+          logger: new DatabaseLogger(),
+        });
         return new DatabaseService(db);
       },
     },
@@ -20,3 +23,12 @@ import { drizzle } from "drizzle-orm/mysql2";
   exports: [...Object.values(DATABASE_PROVIDERS)],
 })
 export class DatabaseModule {}
+
+class DatabaseLogger implements DbLogger {
+  logQuery(query: string, params: unknown[]): void {
+    Logger.debug(
+      `Query: ${query} | Params: ${JSON.stringify(params)}`,
+      "DatabaseLogger",
+    );
+  }
+}
