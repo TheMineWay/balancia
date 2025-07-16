36e0edcc628a6d641d4c601d5f653cb691b6f354
Joel Campos <joelcamposoliva9000@gmail.com>
2025-07-16 11:30:53 +0200

    feat: engines

---
+++ apps/client/src/features/core/role/manager/api/use-role-create.mutation.ts
@@ -0,0 +1,11 @@
+import { useAuthenticatedRequest } from "@common/core/auth/hooks/use-authenticated-request.util";
+import { endpointMutation } from "@common/core/requests/lib/endpoint-mutation.util";
+import { CONTROLLERS } from "@shared/api-definition";
+import { useMutation } from "@tanstack/react-query";
+
+export const useRoleCreateMutation = () => {
+  const { request } = useAuthenticatedRequest();
+  return useMutation({
+    mutationFn: endpointMutation(CONTROLLERS.adminRole, "create", request),
+  });
+};
