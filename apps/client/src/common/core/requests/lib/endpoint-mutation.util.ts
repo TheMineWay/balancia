dda61f06946dca12ae548a8e08a649763fa10e58
Joel Campos Oliva <joelcamposoliva9000@gmail.com>
2025-07-19 19:55:53 +0200

    fix: react usage

--- apps/client/src/common/core/requests/lib/endpoint-mutation.util.ts
+++ apps/client/src/common/core/requests/lib/endpoint-mutation.util.ts
@@ -1,9 +1,9 @@
 import type { RequestOptions } from "@common/core/requests/hooks/use-request.util";
-import { getClientEndpointRequest } from "@common/core/requests/lib/get-client-endpoint-request.util";
+import type { getClientEndpointRequest } from "@common/core/requests/lib/get-client-endpoint-request.util";
 import { ENV } from "@constants/env/env.constant";
 import type {
   ControllerDefinition,
-  InferEndpointDTO,
+  InferBodyDto,
 } from "@shared/api-definition";
 import { getEndpointRequest } from "@shared/api-definition";
 import type { AxiosResponse } from "axios";
@@ -25,7 +25,7 @@ export const endpointMutation = <
   requestFn: (options: RequestOptions) => Promise<AxiosResponse>,
   options?: Parameters<typeof getClientEndpointRequest>[2]
 ) => {
-  return async (data: InferEndpointDTO<C, E>) => {
+  return async (data: InferBodyDto<C, E>) => {
     const { request, onResponse } = getEndpointRequest(
       ENV.api.host,
       controller,
