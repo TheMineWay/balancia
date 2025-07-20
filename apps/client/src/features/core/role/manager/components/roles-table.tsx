fd10ecf0c13714e1ee0460b19936bd4f3790dbab
Joel Campos Oliva <joelcamposoliva9000@gmail.com>
2025-07-20 11:54:10 +0200

    feat: i18n

--- apps/client/src/features/core/role/manager/components/roles-table.tsx
+++ apps/client/src/features/core/role/manager/components/roles-table.tsx
@@ -1,22 +1,30 @@
 import { Table } from "@common/components/table/components/table";
 import { useTable } from "@common/components/table/hooks/use-table";
 import { useAdminRolesQuery } from "@core-fts/role/manager/api/use-admin-roles.query";
+import { useTranslation } from "@i18n/use-translation";
 import type { RoleModel } from "@shared/models";
 
 export const RolesTable: FC = () => {
   const { data: { roles } = {} } = useAdminRolesQuery();
 
+  const { t: commonT } = useTranslation("common");
+  const { t } = useTranslation("role");
+
   const table = useTable<RoleModel>({
     data: roles,
     columns: [
       {
-        label: "Role Name",
+        label: t().models.role.fields.name.Name,
         accessorKey: "name",
       },
       {
-        label: "Created at",
+        label: commonT().models.fields["created-at"].Name,
         accessorKey: "createdAt",
         render: (item) => <p>{item.createdAt.toString()}</p>,
+      },
+      {
+        label: t().admin.table.columns.permissions.Label,
+        render: () => <></>,
       },
     ],
     rowKey: "id",
