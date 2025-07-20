5d8f4f9c49fbc35448400e177cfb6568b1dda72a
Joel Campos Oliva <joelcamposoliva9000@gmail.com>
2025-07-20 12:07:35 +0200

    feat: update OpenAPI

--- apps/server/src/core/api/role/admin-role.controller.ts
+++ apps/server/src/core/api/role/admin-role.controller.ts
@@ -3,6 +3,7 @@ import { Endpoint } from "@core/decorators/endpoints/endpoint.decorator";
 import { ValidatedBody } from "@core/decorators/validation/validated-body.decorator";
 import { Permissions } from "@core/guards/permissions/permission.decorator";
 import { Controller, Param, ParseIntPipe } from "@nestjs/common";
+import { ApiOperation, ApiTags } from "@nestjs/swagger";
 import {
   ADMIN_ROLE_CONTROLLER,
   getController,
@@ -13,16 +14,19 @@ import { Permission } from "@shared/models";
 
 const CONTROLLER = ADMIN_ROLE_CONTROLLER;
 
+@ApiTags("Role administration")
 @Permissions(Permission.ROLE_ADMIN)
 @Controller(getController(CONTROLLER, {}))
 export class AdminRoleController {
   constructor(private readonly roleService: RoleService) {}
 
+  @ApiOperation({ summary: "Get all roles" })
   @Endpoint(CONTROLLER, "get")
   async getRoles(): Promise<InferResponseDto<typeof CONTROLLER, "get">> {
     return { roles: await this.roleService.getAll() };
   }
 
+  @ApiOperation({ summary: "Create a new role" })
   @Endpoint(CONTROLLER, "create")
   async create(
     @ValidatedBody(CONTROLLER, "create")
@@ -31,6 +35,7 @@ export class AdminRoleController {
     await this.roleService.create(body);
   }
 
+  @ApiOperation({ summary: "Update an existing role" })
   @Endpoint(CONTROLLER, "update")
   async update(
     @ValidatedBody(CONTROLLER, "update")
@@ -40,6 +45,7 @@ export class AdminRoleController {
     await this.roleService.update(id, body);
   }
 
+  @ApiOperation({ summary: "Delete a role" })
   @Endpoint(CONTROLLER, "delete")
   async delete(
     @Param("id", ParseIntPipe) id: number,
