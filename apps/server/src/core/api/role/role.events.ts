083b6bea31bf3170cec2ee7ccc308f16230ac920
Joel Campos Oliva <joelcamposoliva9000@gmail.com>
2025-08-04 18:39:16 +0200

    docs: document events

--- apps/server/src/core/api/role/role.events.ts
+++ apps/server/src/core/api/role/role.events.ts
@@ -3,6 +3,9 @@ import { Event } from "src/events/event.abstract";
 
 type RoleUpdatedEventPayload = Pick<RoleModel, "id">;
 
+/**
+ * Event emitted when a role metadata is updated.
+ */
 export class RoleUpdatedEvent extends Event<RoleUpdatedEventPayload> {
   public static readonly NAME = "role.updated";
 
@@ -13,6 +16,9 @@ export class RoleUpdatedEvent extends Event<RoleUpdatedEventPayload> {
 
 type RoleDeletedEventPayload = Pick<RoleModel, "id">;
 
+/**
+ * Event emitted when a role is permanently deleted.
+ */
 export class RoleDeletedEvent extends Event<RoleDeletedEventPayload> {
   public static readonly NAME = "role.deleted";
 
@@ -23,6 +29,9 @@ export class RoleDeletedEvent extends Event<RoleDeletedEventPayload> {
 
 type RoleCreatedEventPayload = Pick<RoleModel, "id">;
 
+/**
+ * Event emitted when a new role is created.
+ */
 export class RoleCreatedEvent extends Event<RoleCreatedEventPayload> {
   public static readonly NAME = "role.created";
 
@@ -36,6 +45,9 @@ type RoleAssignedEventPayload = {
   userId: UserModel["id"];
 };
 
+/**
+ * Event emitted when a role is assigned to a user.
+ */
 export class RoleAssignedEvent extends Event<RoleAssignedEventPayload> {
   public static readonly NAME = "role.assigned";
 
@@ -48,6 +60,10 @@ type RoleUnassignedEventPayload = {
   roleId: RoleModel["id"];
   userId: UserModel["id"];
 };
+
+/**
+ * Event emitted when a role is unassigned from a user.
+ */
 export class RoleUnassignedEvent extends Event<RoleUnassignedEventPayload> {
   public static readonly NAME = "role.unassigned";
 
