ca2489cfda9864fc9d14656067afb3519a9e8eb9
Joel Campos Oliva <joelcamposoliva9000@gmail.com>
2025-08-04 21:28:52 +0200

    feat: protection component

---
+++ apps/client/src/features/core/permission/components/protected.tsx
@@ -0,0 +1,16 @@
+import type { WithChildren } from "@common/types/component/component.types";
+import { useUserInfo } from "@providers/auth/user-info.context";
+import type { Permission } from "@shared/models";
+
+type ProtectedProps = {
+  permissions: Permission[];
+} & WithChildren;
+
+export const Protected: FC<ProtectedProps> = ({ permissions, children }) => {
+  const { permissions: userPermissions } = useUserInfo();
+
+  if (!permissions.every((p) => userPermissions.includes(p))) {
+    return null;
+  }
+  return <>{children}</>;
+};
