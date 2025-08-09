ec58781493f5664763f3df23d07129992b6ea15b
Joel Campos Oliva <joelcamposoliva9000@gmail.com>
2025-08-09 11:48:24 +0200

    fix: type imports

--- apps/server/src/database/repository/core/user.repository.ts
+++ apps/server/src/database/repository/core/user.repository.ts
@@ -1,11 +1,14 @@
-import { QueryOptions, Repository } from "@database/repository/core/repository";
 import {
-	UserInsert,
+	type QueryOptions,
+	Repository,
+} from "@database/repository/core/repository";
+import {
+	type UserInsert,
+	type UserUpdate,
 	userTable,
-	UserUpdate,
 } from "@database/schemas/main/tables/identity/user.table";
 import { Injectable } from "@nestjs/common";
-import { DbUserModel, PaginatedQuery, SearchModel } from "@shared/models";
+import type { DbUserModel, PaginatedQuery, SearchModel } from "@shared/models";
 import { asc, eq, inArray, like } from "drizzle-orm";
 
 @Injectable()
