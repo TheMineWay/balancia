7f52d9fd7e497e3df07d30e2b85019a247ac2768
Joel Campos Oliva <joelcamposoliva9000@gmail.com>
2025-08-09 12:53:09 +0200

    fix: remove checkin endpoint

--- apps/server/src/core/auth/auth/auth.controller.ts
+++ apps/server/src/core/auth/auth/auth.controller.ts
@@ -1,5 +1,4 @@
 import { AuthService } from "@core/auth/auth/auth.service";
-import { Jwt } from "@core/auth/auth/decorators/user/jwt.decorator";
 import { User } from "@core/auth/auth/decorators/user/user.decorator";
 import { Controller } from "@nestjs/common";
 import {
@@ -7,7 +6,7 @@ import {
 	getController,
 	type InferResponseDto,
 } from "@shared/api-definition";
-import type { JwtToken, UserModel } from "@shared/models";
+import type { UserModel } from "@shared/models";
 import { Endpoint } from "src/decorators/endpoints/endpoint.decorator";
 
 const CONTROLLER = AUTH_CONTROLLER;
@@ -15,14 +14,7 @@ const CONTROLLER = AUTH_CONTROLLER;
 @Controller(getController(CONTROLLER, {}))
 export class AuthController {
 	constructor(private readonly authService: AuthService) {}
-
-	@Endpoint(CONTROLLER, "check-in")
-	async checkIn(
-		@Jwt() jwt: JwtToken,
-	): Promise<InferResponseDto<typeof CONTROLLER, "check-in">> {
-		return await this.authService.checkIn(jwt);
-	}
-
+	
 	@Endpoint(CONTROLLER, "my-info")
 	async myInfo(
 		@User() user: UserModel,
