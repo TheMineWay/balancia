25e4a96036204a5e850981486241a7b4cfa6663d
Joel Campos Oliva <joelcamposoliva9000@gmail.com>
2025-08-09 11:10:08 +0200

    feat: remove dev cli

--- scripts/src/cli.ts
+++ scripts/src/cli.ts
@@ -1,25 +1,20 @@
-import { developmentMenu } from "./development";
 import { generate } from "./generate/index";
 import { updateMetadata } from "./update-metadata/index";
 import { selectRunner } from "./utils/select-runner.util.js";
 
 (async () => {
-	console.log("NestFlux tools");
-	await selectRunner(
-		[
-			{
-				run: updateMetadata,
-				label: "Update project metadata",
-			},
-			{
-				run: generate,
-				label: "Generate",
-			},
-			{
-				run: developmentMenu,
-				label: "Development",
-			},
-		],
-		"🛠️ Select tool to run",
-	);
+  console.log("NestFlux tools");
+  await selectRunner(
+    [
+      {
+        run: updateMetadata,
+        label: "Update project metadata",
+      },
+      {
+        run: generate,
+        label: "Generate",
+      },
+    ],
+    "🛠️ Select tool to run"
+  );
 })();
