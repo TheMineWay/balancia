8454d1118ab2356b479771d979fd53a177dfa82f
Joel Campos <joelcamposoliva9000@gmail.com>
2025-08-26 20:37:26 +0200

    feat: add actions

--- apps/client/src/features/finances/transactions/components/transactions-table.tsx
+++ apps/client/src/features/finances/transactions/components/transactions-table.tsx
@@ -3,7 +3,9 @@ import { DatetimeRender } from "@common/extended-ui/date/components/datetime-ren
 import { Table } from "@common/extended-ui/table/components/table";
 import { useTable } from "@common/extended-ui/table/hooks/use-table";
 import { useTranslation } from "@i18n/use-translation";
+import { ActionIcon, Group } from "@mantine/core";
 import { TimePrecision, type TransactionModel } from "@shared/models";
+import { BiEdit, BiTrash } from "react-icons/bi";
 
 type Props = {
     data?: TransactionModel[];
@@ -12,21 +14,38 @@ type Props = {
 
 export const TransactionsTable: FC<Props> = ({ data, loading = false }) => {
     const { t } = useTranslation('transaction');
-    const table = useTable<TransactionModel>({ rowKey: 'id', data, columns: [
-        {
-            label: t().models.transaction.subject.Label,
-            accessorKey: 'subject',
-        },
-        {
-            label: t().models.transaction.amount.Label,
-            accessorKey: 'amount',
-        },
-        {
-            label: t().models.transaction.performedAt.Label,
-            accessorKey: 'performedAt',
-            render: (row) => row.performedAtPrecision === TimePrecision.DATE ? <DateRender date={row.performedAt} /> : <DatetimeRender date={row.performedAt}/>,
-        },
-    ] });
+    const { t: commonT } = useTranslation('common');
 
-    return <Table table={table} loading={loading}/>;
+    const table = useTable<TransactionModel>({
+        rowKey: 'id', data, columns: [
+            {
+                label: t().models.transaction.subject.Label,
+                accessorKey: 'subject',
+            },
+            {
+                label: t().models.transaction.amount.Label,
+                accessorKey: 'amount',
+            },
+            {
+                label: t().models.transaction.performedAt.Label,
+                accessorKey: 'performedAt',
+                render: (row) => row.performedAtPrecision === TimePrecision.DATE ? <DateRender date={row.performedAt} /> : <DatetimeRender date={row.performedAt} />,
+            },
+            {
+                label: commonT().expressions.Actions,
+                render: () => <Group>
+                    <ActionIcon aria-label={commonT().expressions.Edit}>
+                        <BiEdit />
+                    </ActionIcon>
+                    <ActionIcon color="red" aria-label={commonT().expressions.Delete}>
+                        <BiTrash />
+                    </ActionIcon>
+                </Group>,
+            }
+        ]
+    });
+
+    return <div>
+        <Table table={table} loading={loading} classNames={{ root: "max-h-[25rem]" }} />
+    </div>;
 };
