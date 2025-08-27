8fff7127112f65a8f9d6d9661bec3e8106e6ffec
Joel Campos Oliva <joelcamposoliva9000@gmail.com>
2025-08-27 18:27:31 +0200

    fix: use id to ref field

--- apps/client/src/features/finances/transactions/components/forms/transaction.form.tsx
+++ apps/client/src/features/finances/transactions/components/forms/transaction.form.tsx
@@ -3,6 +3,7 @@ import { Form } from "@common/extended-ui/form/components/form";
 import { useTranslation } from "@i18n/use-translation";
 import { Button, Input } from "@mantine/core";
 import { TRANSACTION_MODEL_VALUES, type TransactionCreateModel } from "@shared/models";
+import { useId } from "react";
 import { Controller, type UseFormReturn } from "react-hook-form";
 
 type Props = {
@@ -15,6 +16,7 @@ type Props = {
 
 export const TransactionForm: FC<Props> = ({ form, onSuccess, submitText, submitIcon, loading }) => {
     const { t } = useTranslation('transaction');
+    const amountFieldId = useId();
 
     const { handleSubmit, watch, register, control } = form;
 
@@ -24,8 +26,8 @@ export const TransactionForm: FC<Props> = ({ form, onSuccess, submitText, submit
         <Input.Wrapper label={t().models.transaction.subject.Label}>
             <Input maxLength={TRANSACTION_MODEL_VALUES.subject.maxLength} {...register('subject')}/>
         </Input.Wrapper>
-        <Input.Wrapper label={t().models.transaction.amount.Label}>
-            <Controller control={control} name="amount" render={({ field: {ref, ...restField }}) => <CashInputField {...restField}/>}/>
+        <Input.Wrapper label={t().models.transaction.amount.Label} labelProps={{ htmlFor: amountFieldId }}>
+            <Controller control={control} name="amount" render={({ field: {ref, ...restField }}) => <CashInputField {...restField} id={amountFieldId}/>}/>
         </Input.Wrapper>
 
         <Button disabled={formValues.amount === 0} loading={loading} leftSection={submitIcon} type="submit">
