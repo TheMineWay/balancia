3f72fdcaa7d4fcc7fd0ae8e014f30fee9933f842
Joel Campos Oliva <joelcamposoliva9000@gmail.com>
2025-09-05 15:54:20 +0200

    feat: style chart

--- apps/client/src/features/analytics/my-analytics/components/expenses/monthly-cash-flow.chart.tsx
+++ apps/client/src/features/analytics/my-analytics/components/expenses/monthly-cash-flow.chart.tsx
@@ -18,7 +18,7 @@ export const MonthlyCashFlowChart: FC<Props> = ({ data: rawData = [] }) => {
 	return (
 		<ResponsiveLine
 			data={data}
-			margin={{ top: 50, right: 25, bottom: 50, left: 60 }}
+			margin={{ top: 50, right: 100, bottom: 50, left: 60 }}
 			yScale={{
 				type: "linear",
 				min: "auto",
@@ -27,11 +27,24 @@ export const MonthlyCashFlowChart: FC<Props> = ({ data: rawData = [] }) => {
 				reverse: false,
 			}}
 			axisBottom={{ legend: t().expressions.dates.Month, legendOffset: 36 }}
-			axisLeft={{ legend: t().expressions.Amount, legendOffset: -40 }}
+			axisLeft={{
+				legend: t().expressions.Amount,
+				legendOffset: -40,
+				format: (value) => `${value} €`,
+			}}
 			pointSize={10}
 			pointBorderWidth={1}
-			pointBorderColor={{ from: "seriesColor" }}
 			useMesh={true}
+			legends={[
+				{
+					anchor: "right",
+					direction: "column",
+					translateX: 100,
+					itemWidth: 80,
+					itemHeight: 22,
+					symbolShape: "circle",
+				},
+			]}
 		/>
 	);
 };
