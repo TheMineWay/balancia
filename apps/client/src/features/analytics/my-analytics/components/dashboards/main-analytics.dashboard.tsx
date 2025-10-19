6efb237bbd1aa925d8fe89acc9118fdd8380fd29
Joel Campos <joelcamposoliva9000@gmail.com>
2025-10-19 19:44:54 +0200

    feat: select account id

--- apps/client/src/features/analytics/my-analytics/components/dashboards/main-analytics.dashboard.tsx
+++ apps/client/src/features/analytics/my-analytics/components/dashboards/main-analytics.dashboard.tsx
@@ -3,8 +3,10 @@ import { DateRange } from "@common/extended-ui/date/hooks/use-date-range";
 import { useMonthDateRange } from "@common/extended-ui/date/hooks/use-month-date-range";
 import { useMyAccountMonthlyStatsQuery } from "@fts/analytics/my-analytics/api/use-my-account-monthly-stats.query";
 import { MonthlyCashFlowChart } from "@fts/analytics/my-analytics/components/expenses/monthly-cash-flow.chart";
+import { MyAccountsSelector } from "@fts/finances/accounts/my-accounts/components/form/my-accounts.selector";
 import type { AccountModel } from "@shared/models";
 import { addMonths, subMonths } from "date-fns";
+import { useState } from "react";
 
 const DEFAULT_RANGE: DateRange = {
 	from: addMonths(new Date(), -6),
@@ -16,15 +18,23 @@ type Props = {
 };
 
 export const MainAnalyticsDashboard: FC<Props> = ({ mainAccountId }) => {
+	const [accountId, setAccountId] = useState(mainAccountId);
 	const monthlyStatsRange = useMonthDateRange({
 		initialRange: { from: subMonths(new Date(), 6), to: new Date() },
 	});
-	const { data: monthlyStats } = useMyAccountMonthlyStatsQuery(mainAccountId, {
+	const { data: monthlyStats } = useMyAccountMonthlyStatsQuery(accountId, {
 		range: monthlyStatsRange.range,
 	});
 
 	return (
-		<div className="flex flex-col h-100">
+		<div className="flex flex-col gap-2 h-100">
+			<MyAccountsSelector
+				value={accountId}
+				onChange={(id) => {
+					if (id) setAccountId(id);
+				}}
+				size="xs"
+			/>
 			<MonthRangePicker
 				value={monthlyStatsRange.range}
 				onChange={monthlyStatsRange.setRange}
