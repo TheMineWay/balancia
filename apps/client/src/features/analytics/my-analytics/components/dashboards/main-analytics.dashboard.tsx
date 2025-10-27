099e38196afc73e4e2f33f6e668d7b1d2197bc0a
Joel Campos Oliva <joelcamposoliva9000@gmail.com>
2025-10-27 21:35:34 +0100

    fix: rename charts

--- apps/client/src/features/analytics/my-analytics/components/dashboards/main-analytics.dashboard.tsx
+++ apps/client/src/features/analytics/my-analytics/components/dashboards/main-analytics.dashboard.tsx
@@ -2,7 +2,7 @@ import { MonthRangePicker } from "@common/extended-ui/date/components/range/mont
 import type { DateRange } from "@common/extended-ui/date/hooks/use-date-range";
 import { useMonthDateRange } from "@common/extended-ui/date/hooks/use-month-date-range";
 import { useMyAccountMonthlyStatsQuery } from "@fts/analytics/my-analytics/api/use-my-account-monthly-stats.query";
-import { MonthlyCashFlowChart } from "@fts/analytics/my-analytics/components/expenses/monthly-cash-flow.chart";
+import { MonthlyCashFlowChart } from "@fts/analytics/my-analytics/components/charts/monthly-cash-flow.chart";
 import { MyAccountsSelector } from "@fts/finances/accounts/my-accounts/components/form/my-accounts.selector";
 import { MAX_STATS_MONTH_DATE_DIFF } from "@shared/constants";
 import type { AccountModel } from "@shared/models";
