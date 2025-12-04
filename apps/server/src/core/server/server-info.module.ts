2330ec091edb9e6c433dfbd9c6b1c267901fe7cf
Joel Campos <joelcamposoliva9000@gmail.com>
2025-12-04 22:23:53 +0100

    feat: implement cache on server info

--- apps/server/src/core/server/server-info.module.ts
+++ apps/server/src/core/server/server-info.module.ts
@@ -1,8 +1,10 @@
 import { ServerInfoController } from "@core/server/server-info.controller";
 import { ServerInfoService } from "@core/server/server-info.service";
+import { CacheModule } from "@nestjs/cache-manager";
 import { Module } from "@nestjs/common";
 
 @Module({
+	imports: [CacheModule.register()],
 	providers: [ServerInfoService],
 	exports: [ServerInfoService],
 	controllers: [ServerInfoController],
