dd04c6cad4a27ae757965d32b0ad5fe79836cb6b
Joel Campos <joelcamposoliva9000@gmail.com>
2025-12-05 16:56:08 +0100

    feat: select search improvements

--- apps/client/src/common/extended-ui/form/components/search/select-search.tsx
+++ apps/client/src/common/extended-ui/form/components/search/select-search.tsx
@@ -1,54 +1,109 @@
 import type { UseDebouncedSearch } from "@common/extended-ui/form/components/search/use-debounced-search";
-import { Combobox, TextInput, useCombobox } from "@mantine/core";
-import { useCallback, useMemo } from "react";
+import { useTranslation } from "@i18n/use-translation";
+import {
+	ActionIcon,
+	Combobox,
+	TextInput,
+	type TextInputProps,
+	useCombobox,
+} from "@mantine/core";
+import { useCallback, useEffect, useMemo, useState } from "react";
+import { IoCloseOutline } from "react-icons/io5";
 
-type Data<T extends string | number> = {
+type Data<V> = {
 	label: string;
-	value: T;
+	value: V;
+	disabled?: boolean;
 };
 
-type Props<T extends string | number> = {
-	data: Data<T>[];
+export type SelectSearchProps<T extends string | number, V = T> = {
+	data: Data<V>[];
 	search: UseDebouncedSearch;
+	valueFetch?: (value: T) => Promise<Data<V> | null>;
+	getKey: (item: V) => T;
 
 	// Input
 	placeholder?: string;
 	value?: T | null;
-	setValue?: (value: T | null) => void;
-};
+	setValue?: (value: V | null) => void;
+	allowClear?: boolean;
+	triggerId?: string | number;
+} & Pick<
+	TextInputProps,
+	| "onBlur"
+	| "onFocus"
+	| "onClick"
+	| "size"
+	| "leftSection"
+	| "aria-label"
+	| "className"
+	| "styles"
+>;
 
 /**
  * This component is a search-select input that allows users to select an item from a dropdown list.
  * It includes a search functionality that debounces input changes. As you need to provide the `useDebouncedSearch` hook, you can manage the search state externally.
  */
-export function SelectSearch<T extends string | number>({
-	data,
+export function SelectSearch<T extends string | number, V = T>({
+	data: rawData,
 	placeholder,
 	search,
 	value,
+	valueFetch,
+	getKey,
+	allowClear = false,
 	setValue,
-}: Readonly<Props<T>>) {
+	onBlur,
+	onClick,
+	onFocus,
+	triggerId,
+	...props
+}: Readonly<SelectSearchProps<T, V>>) {
+	const { t } = useTranslation("common");
 	const combobox = useCombobox({});
+
+	const [fetchedData, setFetchedData] = useState<Data<V> | null>(null);
+
+	// Fetch data for the selected value.
+	// In case the selected item is not fetched in the options
+	useEffect(() => {
+		if (!value) setFetchedData(null);
+		else valueFetch?.(value).then((v) => setFetchedData(v ?? null));
+	}, [value, valueFetch]);
+
+	// Add fetched value to the data array
+	const data = useMemo(() => {
+		const cleanData = [...rawData];
+		if (
+			fetchedData &&
+			!rawData.find((d) => getKey(d.value) === getKey(fetchedData.value))
+		)
+			cleanData.push(fetchedData);
+		return cleanData;
+	}, [rawData, fetchedData, getKey]);
 
 	// As the Combobox cannot operate with non-string values directly,
 	// we create a mapping of index to value for the options.
 	const valueMap = useMemo(() => {
-		return data.reduce<[string, T][]>((acc, item, idx) => {
+		return data.reduce<[string, V][]>((acc, item, idx) => {
 			acc.push([idx.toString(), item.value]);
 			return acc;
 		}, []);
 	}, [data]);
 
 	const options = useMemo(() => {
-		return data.map((item) => (
-			<Combobox.Option
-				key={item.value}
-				value={valueMap.find((v) => v[1] === item.value)?.[0] ?? ""}
-			>
-				{item.label}
-			</Combobox.Option>
-		));
-	}, [data, valueMap]);
+		return data
+			.filter((item): item is Data<V> => item !== null)
+			.map((item) => (
+				<Combobox.Option
+					key={getKey(item.value)}
+					value={valueMap.find((v) => v[1] === item.value)?.[0] ?? ""}
+					disabled={item.disabled}
+				>
+					{item.label}
+				</Combobox.Option>
+			));
+	}, [data, valueMap, getKey]);
 
 	const onValueChanged = useCallback(
 		(value: string) => {
@@ -61,8 +116,25 @@ export function SelectSearch<T extends string | number>({
 
 	const selectedOption = useMemo(() => {
 		if (value === undefined || value === null) return null;
-		return data.find((option) => option.value === value) || null;
-	}, [data, value]);
+		return data.find((option) => getKey(option.value) === value) || null;
+	}, [data, value, getKey]);
+
+	const closeComponent = useMemo(
+		() =>
+			allowClear && value ? (
+				<ActionIcon
+					variant="transparent"
+					aria-label={t().expressions.Clear}
+					onClick={() => {
+						setValue?.(null);
+						search.setValue("");
+					}}
+				>
+					<IoCloseOutline />
+				</ActionIcon>
+			) : null,
+		[allowClear, t, setValue, search, value],
+	);
 
 	return (
 		<Combobox
@@ -74,6 +146,7 @@ export function SelectSearch<T extends string | number>({
 		>
 			<Combobox.Target>
 				<TextInput
+					rightSection={closeComponent}
 					value={selectedOption ? selectedOption.label : search.value}
 					onChange={(e) => {
 						combobox.openDropdown();
@@ -81,9 +154,21 @@ export function SelectSearch<T extends string | number>({
 						search.setValue(e.currentTarget.value);
 					}}
 					placeholder={placeholder}
-					onClick={() => combobox.openDropdown()}
-					onFocus={() => combobox.openDropdown()}
-					onBlur={() => combobox.closeDropdown()}
+					onClick={(e) => {
+						combobox.openDropdown();
+						onClick?.(e);
+					}}
+					onFocus={(e) => {
+						combobox.openDropdown();
+						onFocus?.(e);
+					}}
+					onBlur={(e) => {
+						combobox.closeDropdown();
+						search.setValue("");
+						onBlur?.(e);
+					}}
+					id={triggerId?.toString()}
+					{...props}
 				/>
 			</Combobox.Target>
 
