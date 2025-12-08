d01223b347ee4655fa309af74ba3a351f64bc584
Joel Campos <joelcamposoliva9000@gmail.com>
2025-12-08 14:43:27 +0100

    feat: common action props

--- apps/client/src/common/extended-ui/table/constants/table.constants.ts
+++ apps/client/src/common/extended-ui/table/constants/table.constants.ts
@@ -1,6 +1,31 @@
+import { ACTION_SHARED_PROPS } from "@common/extended-ui/button/actions/constants/action-shared-props.constant";
 import type { ActionIconProps } from "@mantine/core";
 
+type PropTypes = keyof typeof ACTION_SHARED_PROPS;
+
+/**
+ * Props for action buttons used in tables.
+ */
 export const TABLE_ACTION_PROPS = {
-	size: "md",
-	variant: "light",
-} satisfies ActionIconProps;
+	default: {
+		...ACTION_SHARED_PROPS.default,
+	},
+	primary: {
+		...ACTION_SHARED_PROPS.primary,
+	},
+	danger: {
+		...ACTION_SHARED_PROPS.danger,
+	},
+	success: {
+		...ACTION_SHARED_PROPS.success,
+	},
+	warning: {
+		...ACTION_SHARED_PROPS.warning,
+	},
+	info: {
+		...ACTION_SHARED_PROPS.info,
+	},
+	ghost: {
+		...ACTION_SHARED_PROPS.ghost,
+	},
+} satisfies Record<PropTypes, ActionIconProps>;
