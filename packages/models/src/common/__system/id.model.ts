14be8b70cc53261f76cdfa8b4fceefa3f5fed69f
Joel Campos <joelcamposoliva9000@gmail.com>
2025-12-24 14:01:04 +0100

    fix: use coerce

--- packages/models/src/common/__system/id.model.ts
+++ packages/models/src/common/__system/id.model.ts
@@ -1,8 +1,5 @@
 import z from "zod";
 
-export const ID_SCHEMA = z.preprocess((val) => {
-	if (typeof val === "string") return parseInt(val, 10);
-	return val;
-}, z.number().int().positive());
+export const ID_SCHEMA = z.coerce.number().int().positive();
 
 export type IdModel = z.infer<typeof ID_SCHEMA>;
