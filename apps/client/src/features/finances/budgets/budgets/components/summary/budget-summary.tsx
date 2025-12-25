ef06f097c83eefc3fdf421914ccd3647813cb914
Joel Campos <joelcamposoliva9000@gmail.com>
2025-12-25 14:12:20 +0100

    feat: add budget metadata

--- apps/client/src/features/finances/budgets/budgets/components/summary/budget-summary.tsx
+++ apps/client/src/features/finances/budgets/budgets/components/summary/budget-summary.tsx
@@ -1,5 +1,5 @@
 import { BudgetSegmentsUsageChart } from "@fts/finances/budgets/charts/budget-segments-usage.chart";
-import { Title } from "@mantine/core";
+import { Divider, Text, Title } from "@mantine/core";
 import type { BudgetModel, BudgetSegmentModel } from "@shared/models";
 
 type Props = {
@@ -11,9 +11,11 @@ export const BudgetSummary: FC<Props> = ({ budget, segments }) => {
 	return (
 		<div className="flex flex-col gap-2">
 			<Title>{budget.name}</Title>
+			{budget.description && <Text>{budget.description}</Text>}
+			<Divider />
 
 			{/* Segment usage chart */}
-			<div className="h-20">
+			<div className="h-20 my-2">
 				<BudgetSegmentsUsageChart segments={segments} />
 			</div>
 		</div>
