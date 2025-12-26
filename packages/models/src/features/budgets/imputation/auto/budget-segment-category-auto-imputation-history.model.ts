8f2f1d390bee474ceb1a5d09288ca3cdcbe807a9
Joel Campos <joelcamposoliva9000@gmail.com>
2025-12-26 12:23:46 +0100

    feat: imputation models

---
+++ packages/models/src/features/budgets/imputation/auto/budget-segment-category-auto-imputation-history.model.ts
@@ -0,0 +1,34 @@
+import { ID_SCHEMA } from "@/common/__system/id.model";
+import { BUDGET_SEGMENT_IMPUTATION_SCHEMA } from "@/features/budgets/imputation/budget-segment-imputation.model";
+import { CATEGORY_SCHEMA } from "@/features/finances/category/category.model";
+import { TIMESTAMPS_SCHEMA } from "@/utils/timestamps.model";
+import z from "zod";
+
+/**
+ * Budget Segment Auto Imputation History Model
+ * Represents the history of automatic imputations for budget segment by categories.
+ */
+export const BUDGET_SEGMENT_CATEGORY_AUTO_IMPUTATION_HISTORY_SCHEMA = z.object({
+	id: ID_SCHEMA,
+
+	//References
+	categoryId: CATEGORY_SCHEMA.shape.id,
+	imputationId: BUDGET_SEGMENT_IMPUTATION_SCHEMA.shape.id,
+
+	// Timestamps
+	...TIMESTAMPS_SCHEMA.shape,
+});
+export type BudgetSegmentCategoryAutoImputationHistoryModel = z.infer<
+	typeof BUDGET_SEGMENT_CATEGORY_AUTO_IMPUTATION_HISTORY_SCHEMA
+>;
+
+/* Create */
+export const BUDGET_SEGMENT_CATEGORY_AUTO_IMPUTATION_HISTORY_CREATE_SCHEMA =
+	BUDGET_SEGMENT_CATEGORY_AUTO_IMPUTATION_HISTORY_SCHEMA.omit({
+		id: true,
+		createdAt: true,
+		updatedAt: true,
+	});
+export type BudgetSegmentCategoryAutoImputationHistoryCreateModel = z.infer<
+	typeof BUDGET_SEGMENT_CATEGORY_AUTO_IMPUTATION_HISTORY_CREATE_SCHEMA
+>;
---
+++ packages/models/src/features/budgets/imputation/budget-segment-imputation.model.ts
@@ -0,0 +1,52 @@
+import { ID_SCHEMA } from "@/common/__system/id.model";
+import { BUDGET_SEGMENT_SCHEMA } from "@/features/budgets/budget-segment.model";
+import { TRANSACTION_SCHEMA } from "@/features/finances/transactions/transaction.model";
+import { nullableStringTransform } from "@/utils/nullable-string.model";
+import { TIMESTAMPS_SCHEMA } from "@/utils/timestamps.model";
+import { ModelValues } from "@ts-types/model-values.type";
+import z from "zod";
+
+export const BUDGET_SEGMENT_IMPUTATION_MODEL_VALUES = {
+	description: {
+		maxLength: 1024,
+	},
+} satisfies ModelValues;
+
+/**
+ * Budget Segment Imputation Model
+ * Represents the association between a budget segment and a transaction.
+ */
+export const BUDGET_SEGMENT_IMPUTATION_SCHEMA = z.object({
+	id: ID_SCHEMA,
+
+	// References
+	segmentId: BUDGET_SEGMENT_SCHEMA.shape.id,
+	transactionId: TRANSACTION_SCHEMA.shape.id,
+
+	// Metadata
+	description: z.preprocess(
+		nullableStringTransform,
+		z
+			.string()
+			.max(BUDGET_SEGMENT_IMPUTATION_MODEL_VALUES.description.maxLength)
+			.nullable()
+			.default(null),
+	),
+
+	// Timestamps
+	...TIMESTAMPS_SCHEMA.shape,
+});
+export type BudgetSegmentImputationModel = z.infer<
+	typeof BUDGET_SEGMENT_IMPUTATION_SCHEMA
+>;
+
+/* Create */
+export const BUDGET_SEGMENT_IMPUTATION_CREATE_SCHEMA =
+	BUDGET_SEGMENT_IMPUTATION_SCHEMA.omit({
+		id: true,
+		createdAt: true,
+		updatedAt: true,
+	});
+export type BudgetSegmentImputationCreateModel = z.infer<
+	typeof BUDGET_SEGMENT_IMPUTATION_CREATE_SCHEMA
+>;
